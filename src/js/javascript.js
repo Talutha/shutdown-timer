3b443e2c8a1ce1b5c545d3dcdde33211544858cd
Marvin Warren <talutha@gmail.com>
2016-06-11 14:26:13 -0500

    Setup timer div

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -1,3 +1,14 @@
-function sayHello() {
-  alert('Howdy!');
-}
+var timerCount = 15;
+
+window.onload = function() {
+  var timerID = document.getElementById('timer');
+  timerID.innerHTML += timerCount;
+};
+
+function startTimer() {
+
+};
+
+function endTimer() {
+
+};
