c0ff4c67a0c3781373ef8ae12c9098c5af584b89
Marvin Warren <talutha@gmail.com>
2016-06-11 15:34:51 -0500

    Setup timer function

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -5,8 +5,31 @@ window.onload = function() {
   timerID.innerHTML += timerCount;
 };
 
-function startTimer() {
+var TimerFunc = function() {
+  window.onload = function() {
+    this.timerID = document.getElementById('timer');
+  };
+};
 
+TimerFunc.prototype.startTimer = function() {
+  this.countdown = window.setInterval(function() {
+    timerCount--;
+    this.timerID.innerHTML = timerCount;
+  }, 1000);
+};
+
+TimerFunc.prototype.endTimer = function() {
+  clearInterval(this.countdown);
+};
+
+var timer = new TimerFunc();
+
+function startTimer() {
+  var timerID = document.getElementById('timer');
+  window.setInterval(function() {
+    timerCount--;
+    timerID.innerHTML = timerCount;
+  }, 1000);
 };
 
 function endTimer() {
