b91ddf1d72ffc96aacd5ba68c0aa63a5d3c79231
Marvin Warren <talutha@gmail.com>
2016-06-18 17:11:02 -0500

    Attempt to get background to change with time remaining

--- src/js/javascript.js
+++ src/js/javascript.js
@@ -20,7 +20,7 @@ var TimerFunc = function() {
     this.timerID = document.getElementById('timeForm');
     // Disply initial timer length, currently in seconds
     this.timerID.value = self.numeral(timerCount).format('00:00:00');
-
+    document.getElementById('startTimerButton').addEventListener('click', buttonTimerClick);
     if (debug) {
       // document.getElementById('titleHeader').innerHTML += '**DEBUG**';
     };
@@ -30,13 +30,24 @@ var TimerFunc = function() {
 // Function to start countdown, decrements timerCount in seconds
 TimerFunc.prototype.startTimer = function() {
   var self = this;
+  var bgState = 3;
   timerCount = self.numeral().unformat(document.getElementById('timeForm').value);
   if (timerCount > 0 && !self.timerRunning) {
-    changeBGColor(timerCount);
     self.hideButtons();
     self.timerRunning = true;
     self.countdown = window.setInterval(function() {
       timerCount--;
+      console.log(bgState);
+      if (timerCount > 60 * 5 && bgState === 3) {
+        changeBGColor(timerCount);
+        bgState--;
+      } else if (timerCount > 60 && timerCount <= (60 * 5) && bgState === 2) {
+        changeBGColor(timerCount);
+        bgState--;
+      } else if (timerCount <= 60 && bgState === 1) {
+        changeBGColor(timerCount);
+        bgState--;
+      };
       this.timerID.value = self.numeral(timerCount).format('00:00:00');
       if (timerCount <= 0) {
         self.endTimer();
@@ -91,27 +102,43 @@ TimerFunc.prototype.hideButtons = function() {
 
 var timer = new TimerFunc();
 
+// To do background transitions depending on time remaining
 function changeBGColor(time) {
+  // five minutes
   var fiveMin = 60 * 5;
+  // one minute
   var oneMin = 60;
+  // element selector for body style
   var body = document.body.style;
+  var flexCont = document.getElementById('flexContainer');
   if (time > fiveMin) {
-    body.transition = 'background-color 5s ease-in-out';
-    body.backgroundColor = '#27e833';
+    // set background color to green over 5 seconds
+    flexCont.style.transition = 'background-color 4s ease-in-out';
+    flexCont.style.backgroundColor = '#27e833';
     timeRemaining = (time - fiveMin - 5).toString();
-    document.body.addEventListener('transitionend', function() {
-      body.transition = 'background-color ' + timeRemaining + 's ease-in-out';
-      body.backgroundColor = 'yellow';
-      timeRemaining = (fiveMin - oneMin - 5).toString();
-      document.body.addEventListener('transitionend', function() {
-        body.transition = 'background-color ' + timeRemaining + 's ease-in-out';
-        body.backgroundColor = 'red';
-      }, false);
-    }, false);
-  } else if (time >= oneMin && time <= fiveMin) {
-    // Yellow background
+    // wait until first transition is done then ease color to Yellow
+    // for remaining time above 5 minutes
+
+    function toYellow(e) {
+      flexCont.style.transition = 'background-color ' + timeRemaining + 's ease-in-out';
+      flexCont.style.backgroundColor = '#fad201';
+      console.log(e);
+      e.stopPropagation();
+      flexCont.removeEventListener('transitionend', toYellow, false);
+    };
+
+    flexCont.addEventListener('transitionend', toYellow, false);
+  } else if (time > oneMin && time <= fiveMin) {
+    flexCont.style.transition = 'background-color 4s ease-in-out';
+    flexCont.style.backgroundColor = '#fad201';
+    timeRemaining = (time - 60 - 5).toString();
+    flexCont.addEventListener('transitionend', function() {
+      flexCont.style.transition = 'background-color ' + timeRemaining + 's ease-in-out';
+      flexCont.style.backgroundColor = '#f9152f';
+    }, true);
   } else if (time < oneMin) {
-    // Red background
+    flexCont.style.transition = 'background-color 4s ease-in-out';
+    flexCont.style.backgroundColor = '#f9152f';
   } else {
     alert('You have broken the properties of TIME!');
   };
@@ -138,4 +165,10 @@ function shutdown() {
   } else {
     exec(cmd, function(error, stdout, stderr) {});
   };
+};
+
+function buttonTimerClick(e) {
+  console.log(e);
+  timer.startTimer();
+  e.stopPropagation();
 };
